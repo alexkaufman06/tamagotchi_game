ce3efa384cab0b27939379a35a12d6d7223ea2ba
“Gabe <alexkaufman06@gmail.com>
2015-02-03 14:28:46 -0800

    Add two player option

--- app.rb
+++ app.rb
@@ -12,11 +12,11 @@ post '/' do
   redirect back
 end
 
-get '/game/:id' do
+get '/single_game/:id' do
   @tamagotchi = Tamagotchi.find(params['id'].to_i)
   @tamagotchi.time_passes(1)
   if @tamagotchi.is_alive?
-    erb :game
+    erb :single_game
   else
     @name = @tamagotchi.name
     @tamagotchi.destroy
@@ -53,4 +53,31 @@ get '/delete/:id' do
   to_delete = Tamagotchi.find(params['id'].to_i)
   to_delete.destroy
   redirect '/'
+end
+
+get '/load_game' do
+  tamagotchis = params['tamagotchis']
+  if tamagotchis.length == 1
+    redirect ('/single_game/' + tamagotchis[0])
+  elsif tamagotchis.length == 2
+    redirect ('/double_game/' + tamagotchis[0] + "/" + tamagotchis[1])
+  else
+
+  end
+end
+
+get '/double_game/:id1/:id2' do
+  @tamagotchi1 = Tamagotchi.find(params['id1'])
+  @tamagotchi2 = Tamagotchi.find(params['id2'])
+  @tamagotchi1.time_passes(1)
+  @tamagotchi2.time_passes(1)
+  if @tamagotchi1.is_alive? || @tamagotchi2.is_alive?
+    erb :double_game
+  else
+    @name1 = @tamagotchi1.name
+    @name2 = @tamagotchi2.name
+    @tamagotchi1.destroy
+    @tamagotchi2.destroy
+    erb :double_dead
+  end
 end
